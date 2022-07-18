5e879cf8b86ea378e15663bc9e3f469881b2c312
MartinjasMarta <109541501+MartinjasMarta@users.noreply.github.com>
2022-07-18 18:21:59 +0200

    Add files via upload

---
+++ EPUB/script/script.js
@@ -0,0 +1,32 @@
+function promijeniBoju() 
+{ 
+    document.getElementById("prvi").style.color = "green";
+    document.getElementById("drugi").style.color = "red";
+    document.getElementById("treci").style.color = "orange";
+    document.getElementById("cetvrti").style.color = "blue";
+    document.getElementById("peti").style.color = "purple";
+
+}
+
+function promijeniFont () 
+{
+    document.getElementById("prvi").style.fontSize = "30px";
+    document.getElementById("drugi").style.fontSize = "30px";
+    document.getElementById("treci").style.fontSize = "30px";
+    document.getElementById("cetvrti").style.fontSize = "30px";
+    document.getElementById("peti").style.fontSize = "30px";
+}
+
+function vrati () 
+{
+    document.getElementById("prvi").style.color = "black";
+    document.getElementById("drugi").style.color = "black";
+    document.getElementById("treci").style.color = "black";
+    document.getElementById("cetvrti").style.color = "black";
+    document.getElementById("peti").style.color = "black";
+    document.getElementById("prvi").style.fontSize = "16px";
+    document.getElementById("drugi").style.fontSize = "16px";
+    document.getElementById("treci").style.fontSize = "16px";
+    document.getElementById("cetvrti").style.fontSize = "16px";
+    document.getElementById("peti").style.fontSize = "16px";
+}
